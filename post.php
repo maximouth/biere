ba87c8695eefdaf48a7d727f0a7292d7c4523c92
root <root@localhost.localdomain>
2015-11-22 16:35:41 +0100

    coincé sur l'ecriture..

--- post.php
+++ post.php
@@ -2,6 +2,7 @@
 
 include "controleSaisies.php";
 include "formulaire.php";
+include "masque.php";
 
 $type = saisie_fiable($_POST, 'type','',false);
 $couleur = saisie_fiable($_POST, 'couleur', '', false);
@@ -11,21 +12,47 @@ $recette = saisie_fiable($_POST, 'recette', '', false);
 
 if (!$type OR !$couleur OR !$auteur OR !$biere OR !$recette ) {
   if (!$recette) {
+    echo masque();
+    echo "<div class=pres>";
     echo formulaire ();
+    echo "</div></body></html>";
   }
   else {
+    echo masque();
+    echo "<div class=pres>";
     echo formulaire2 ($_POST['recette']);
+    echo "</div></body></html>";
   }
 }
 
 else {
-  $file = fopen ($_POST['type']."/".$_POST['$couleur']."/".$_POST['$auteur']."_".$_POST['biere'].".txt" , 'a+');
 
-  fputs ($file, htmlspecilchar($recette));  
+    echo masque();
+
+
+    ///**** ICI ****///
+    $file = fopen ("test.txt", 'c');
+    if (!$file) {
+      echo "<h1>ouverture ratée</h1>";
+    }
+
+
+    //.$_POST['type']."/".$_POST['couleur']."/".$_POST['auteur']."_".$_POST['biere'], 'a+');
+
+  
+  if (fwrite ($file, 'test') === false) {
+      echo "<h1>ecriture ratée</h1>";
+  }
+
+  fwrite ($file, htmlspecilchar($_POST['recette']));  
 
   flose ($file);
 
-  echo "<h1>formulaire plein  </h1>";
+    echo "<div class=pres>";
+    echo "<h1>recette soumise</h1>";
+    echo "</div></body></html>";
+
+ 
 }
 
 
